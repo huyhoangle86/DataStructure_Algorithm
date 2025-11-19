03975f37d0c725f37806f408149e4d2edd50a8e0
huyhoangle <huyhoangle8696@gmail.com>
2025-11-19 11:24:36 +0700

    commit bst

--- bst.py
+++ bst.py
@@ -29,6 +29,20 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def __r_insert(self, current_node, value):
+        if current_node == None: 
+            return Node(value)   
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value) 
+        return current_node    
+
+    def r_insert(self, value):
+        if self.root == None: 
+            self.root = Node(value)
+        self.__r_insert(self.root, value) 
+    
     def contains(self, value):
         temp = self.root
         while temp is not None:
@@ -39,13 +53,44 @@ class BinarySearchTree:
             else:
                 return True
         return False
+    
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+    
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
 
 my_tree = BinarySearchTree()
 
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+# print(my_tree.root.value)
+# print(my_tree.root.left.value)
+# print(my_tree.root.right.value)
+
+# print('BST contains 27:')
+# print(my_tree.r_contains(27))
+
+# print('\n BST Contains 17:')
+# print(my_tree.r_contains(17))
+
+my_tree.r_insert(2)
+my_tree.r_insert(1)
+my_tree.r_insert(3)
+
+print('Root: ', my_tree.root.value)
+print('Root left: ', my_tree.root.left.value)
+print('Root right: ', my_tree.root.right.value)
