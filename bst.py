f92ec17cfe9d7139e0e75ed9524bbe9b22374063
huyhoangle <huyhoangle8696@gmail.com>
2025-11-20 15:16:53 +0800

    added recursive delete

--- bst.py
+++ bst.py
@@ -66,8 +66,52 @@ class BinarySearchTree:
     
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
+    
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+    def __delete_node(self, current_node, value):
+        if current_node == None: 
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value: 
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            
+            else:
+                
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
 
 my_tree = BinarySearchTree()
+my_tree.r_insert(2)
+my_tree.r_insert(1)
+my_tree.r_insert(3)
+
+print('root:', my_tree.root.value)
+print('root.left:', my_tree.root.left.value)
+print('root.right:', my_tree.root.right.value)
+
+my_tree.delete_node(2)
+
+print('root:', my_tree.root.value)
+print('root.left:', my_tree.root.left.value)
+print('root.right:', my_tree.root.right.value)
 
 # my_tree.insert(47)
 # my_tree.insert(21)
